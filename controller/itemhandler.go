4bd9d8140b582250ced758ebc94faf9bad2e7a8c
ShuwenHe <1201220707@pku.edu.cn>
2020-12-05 11:49:04 +0800

    feat: shuwen-shop

---
+++ controller/itemhandler.go
@@ -0,0 +1,111 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+	"text/template"
+
+	"github.com/shuwenhe/shuwen-shop/model"
+	"github.com/shuwenhe/shuwen-shop/service"
+)
+
+func GetItems(w http.ResponseWriter, r *http.Request) {
+	result := service.GetItems()
+	t := template.Must(template.ParseFiles("views/pages/manager/book_manager.html"))
+	t.Execute(w, result)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	bookId := r.FormValue("bookId")
+	service.DeleteItem(bookId)
+	GetPageBooks(w, r)
+}
+
+// ToUpdateBookPage Go to update or add book page
+func ToUpdateBookPage(w http.ResponseWriter, r *http.Request) {
+	bookID := r.FormValue("bookId")        // Get the id of the book to be updated
+	book, _ := service.GetItemByID(bookID) // Call the function of getting books in bookdao
+	if book.ID > 0 {
+		t := template.Must(template.ParseFiles("views/pages/manager/book_edit.html")) // Parse the template
+		t.Execute(w, book)                                                            // Execute
+	} else {
+		t := template.Must(template.ParseFiles("views/pages/manager/book_edit.html")) // 解析模板
+		t.Execute(w, "")                                                              // 执行
+	}
+}
+
+// UpdateOrAddBook 更新或添加图书
+func UpdateOrAddBook(w http.ResponseWriter, r *http.Request) {
+	bookID := r.PostFormValue("bookId") // 获取图书信息
+	title := r.PostFormValue("title")
+	author := r.PostFormValue("author")
+	price := r.PostFormValue("price")
+	sales := r.PostFormValue("sales")
+	stock := r.PostFormValue("stock")
+	fPrice, _ := strconv.ParseFloat(price, 64) // 将价格、销量、库存进行转换
+	iSales, _ := strconv.ParseInt(sales, 10, 0)
+	iStock, _ := strconv.ParseInt(stock, 10, 0)
+	iBookID, _ := strconv.ParseInt(bookID, 10, 0)
+	book := &model.Item{ // 创建book
+		ID:      int(iBookID),
+		Title:   title,
+		Author:  author,
+		Price:   fPrice,
+		Sales:   iSales,
+		Stock:   iStock,
+		ImgPath: "/static/img/default.jpg",
+	}
+	if book.ID > 0 {
+		service.UpdateItem(book) // 调用更新图书的函数
+	} else {
+		service.AddItem(book)
+	}
+	GetPageBooks(w, r)
+}
+
+// GetPageBooks 获取分页图书
+func GetPageBooks(w http.ResponseWriter, r *http.Request) {
+	pageNo := r.FormValue("pageNo")
+	if pageNo == "" {
+		pageNo = "1"
+	}
+	page, _ := service.GetPageItems(pageNo)
+	t := template.Must(template.ParseFiles("views/pages/manager/book_manager.html"))
+	t.Execute(w, page)
+}
+
+// GetPageBooksByPrice Get a book with pagination and price range
+func GetPageBooksByPrice(w http.ResponseWriter, r *http.Request) {
+	pageNo := r.FormValue("pageNo")
+	minPrice := r.FormValue("min") // The difference between FormValue and PostFormValue？
+	maxPrice := r.FormValue("max") // The hyperlink is not a POST request
+	if pageNo == "" {
+		pageNo = "1"
+	}
+	var page *model.Page
+	if minPrice == "" && maxPrice == "" {
+		page, _ = service.GetPageItems(pageNo) // Call the function of bookdao to get the book with pagination
+	} else {
+		page, _ = service.GetPageItemsByPrice(pageNo, minPrice, maxPrice)
+		page.MinPrice = minPrice // Set the price range to the page
+		page.MaxPrice = maxPrice
+	}
+	flag, session := service.IsLogin(r) // Call IsLogin to determine whether the user has logged in
+	if flag {
+		page.IsLogin = true // The user has logged in, set the IsLogin field and Username field values in the page
+		page.Username = session.UserName
+	}
+	t := template.Must(template.ParseFiles("views/index.html")) // Parse the template
+	t.Execute(w, page)                                          
+}
+
+// Indexhandler Get homepage
+func Indexhandler(w http.ResponseWriter, r *http.Request) {
+	pageNo := r.FormValue("pageNo") // Get page number
+	if pageNo == "" {
+		pageNo = "1"
+	}
+	page, _ := service.GetPageItems(pageNo) // Call the function of bookdao to get the book with pagination
+	t := template.Must(template.ParseFiles("views/index.html"))
+	t.Execute(w, page)
+}
---
+++ controller/userhandler.go
@@ -0,0 +1,81 @@
+package controller
+
+import (
+	"net/http"
+	"text/template"
+
+	"github.com/shuwenhe/shuwen-shop/model"
+	"github.com/shuwenhe/shuwen-shop/service"
+	"github.com/shuwenhe/shuwen-shop/utils"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	flag, _ := service.IsLogin(r) // Determine whether you have logged in
+	if flag {
+		GetPageBooksByPrice(w, r) // Go to homepage
+	} else {
+		username := r.PostFormValue("username") // Get username
+		password := r.PostFormValue("password") // Get password
+		user, _ := service.CheckUserNameAndPassword(username, string(password))
+		if user.ID > 0 { // Username and password are correct
+			uuid := utils.CreateUUID()    // Generate UUID
+			session := &model.Session{ // Create a session
+			SessionID: uuid,
+				UserName:  user.Username,
+				UserID:    user.ID,
+			}
+			service.AddSession(session) // Write the session to the database, there is an identification in the database
+			cookie := http.Cookie{      // Create a cookie associated with the session
+				Name:     "user",
+				Value:    uuid,
+				HttpOnly: true,
+			}
+			http.SetCookie(w, &cookie)// Send cookie to browser
+			t := template.Must(template.ParseFiles("views/pages/user/login_success.html"))
+			t.Execute(w, user)
+		} else {
+			t := template.Must(template.ParseFiles("views/pages/user/login.html"))
+			t.Execute(w, "Incorrect username or password")
+		}
+	}
+}
+
+// Logout Logout
+func Logout(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := r.Cookie("user")
+	if cookie != nil {
+		cookieValue := cookie.Value // Get the value of the cookie
+		service.DeleteSession(cookieValue)
+		cookie.MaxAge = -1        // Set cookie invalidation
+		http.SetCookie(w, cookie) // Send the modified cookie to the browser
+	}
+	GetPageBooksByPrice(w, r) // Go to homepage
+}
+
+func Regist(w http.ResponseWriter, r *http.Request) {
+	username := r.PostFormValue("username")
+	password := r.PostFormValue("password")
+	email := r.PostFormValue("email")
+	// TODO Password encryption
+	// hasedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost) // Encrypt the user's password when creating a user
+	user, _ := service.CheckUserName(username)
+	if user.ID > 0 {
+		t := template.Must(template.ParseFiles("views/pages/user/regist.html"))
+		t.Execute(w, "Username already exists")
+	} else {
+		service.SaveUser(username, password, email)
+		t := template.Must(template.ParseFiles("views/pages/user/regist_success.html"))
+		t.Execute(w, "Incorrect username or password")
+	}
+}
+
+// CheckUserName Verify that the username exists by sending an Ajax request
+func CheckUserName(w http.ResponseWriter, r *http.Request) {
+	username := r.PostFormValue("username")
+	user, _ := service.CheckUserName(username)
+	if user.ID > 0 {
+		w.Write([]byte("Username already exists！"))
+	} else {
+		w.Write([]byte("<font style = 'color:green'>Username does not exist！</font>"))
+	}
+}
